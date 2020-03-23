b3ec6891acd976d1423c433aee837ea5207c29dc
Daniel Vetter <daniel.vetter@ffwll.ch>
2020-03-23 15:49:00 +0100

    UPSTREAM: mm/sl[uo]b: export __kmalloc_track(_node)_caller
    slab does this already, and I want to use this in a memory allocation
    tracker in drm for stuff that's tied to the lifetime of a drm_device,
    not the underlying struct device. Kinda like devres, but for drm.
    
    Acked-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Daniel Vetter <daniel.vetter@intel.com>
    Cc: Christoph Lameter <cl@linux.com>
    Cc: Pekka Enberg <penberg@kernel.org>
    Cc: David Rientjes <rientjes@google.com>
    Cc: Joonsoo Kim <iamjoonsoo.kim@lge.com>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: linux-mm@kvack.org
    Link: https://patchwork.freedesktop.org/patch/msgid/20200323144950.3018436-2-daniel.vetter@ffwll.ch
    (cherry picked from commit fd7cb5753ef49964ea9db5121c3fc9a4ec21ed8e)
    Bug: 163141236
    Signed-off-by: Alistair Delva <adelva@google.com>
    Change-Id: I10790befc779311a5f2ee441e4d073e51a5a7a62

--- mm/slub.c
+++ mm/slub.c
@@ -4537,6 +4537,7 @@ void *__kmalloc_track_caller(size_t size, gfp_t gfpflags, unsigned long caller)
 
 	return ret;
 }
+EXPORT_SYMBOL(__kmalloc_track_caller);
 
 #ifdef CONFIG_NUMA
 void *__kmalloc_node_track_caller(size_t size, gfp_t gfpflags,
@@ -4567,6 +4568,7 @@ void *__kmalloc_node_track_caller(size_t size, gfp_t gfpflags,
 
 	return ret;
 }
+EXPORT_SYMBOL(__kmalloc_node_track_caller);
 #endif
 
 #ifdef CONFIG_SYSFS
